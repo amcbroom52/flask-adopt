22b3a2e6fa539766a79f1b5e3828c2d78f7011f0
Cooper Spinelli <cooperspinelli@gmail.com>
2024-03-01 16:07:16 -0700

    Add form and endpoints for displaying form and handling form submit

--- app.py
+++ app.py
@@ -2,9 +2,9 @@
 
 import os
 
-from flask import Flask
+from flask import Flask, render_template, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
-
+from forms import AddPetForm
 from models import connect_db, db, Pet
 
 app = Flask(__name__)
@@ -22,3 +22,44 @@ connect_db(app)
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
+
+
+@app.get('/')
+def display_homepage():
+    """Displays homepage"""
+
+    pets = Pet.query.all()
+    return render_template("homepage.html", pets = pets)
+
+
+
+@app.route("/add", methods=['GET', 'POST'])
+def add_pet():
+    """Show user add pet form and handle adding of pet"""
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        new_pet = Pet(
+            name=form.name.data,
+            species=form.species.data,
+            photo_url=form.photo_url.data,
+            age=form.age.data,
+            notes=form.notes.data
+        )
+
+        db.session.add(new_pet)
+        db.session.commit()
+
+        flash("Pet added!")
+        return redirect('/')
+    else:
+        return render_template('add_pet_form.html', form=form)
+
+
+
+
+
+
+
+
--- forms.py
+++ forms.py
@@ -1,1 +1,32 @@
 """Forms for adopt app."""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, URLField, SelectField
+from wtforms.validators import InputRequired
+
+
+class AddPetForm(FlaskForm):
+    """Form for adding pet"""
+
+    name = StringField(
+        "Pet Name",
+        validators=[InputRequired()])
+
+    species = StringField(
+        "Species",
+        validators=[InputRequired()])
+
+    photo_url = URLField(
+        "Photo URL")
+
+    age = SelectField(
+        "Age",
+        choices=[('baby', 'Baby'),
+                 ('young', 'Young'),
+                 ('adult', 'Adult'),
+                 ('senior', 'Senior')],
+        validators=[InputRequired()])
+
+    notes = StringField("Notes")
+
+
